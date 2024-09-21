49a2bb27682afdd82c45ca68b143742cd2368d08
yuchi <chronictanvi@gmail.com>
2024-09-20 23:28:20 -0400

    method 1
    with just GA tracking , no GTM

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,8 @@ import Footer from '@/components/Footer'
 import siteMetadata from '@/data/siteMetadata'
 import { ThemeProviders } from './theme-providers'
 import { Metadata } from 'next'
-import { GoogleTagManager } from '@next/third-parties/google'
+// import { GoogleTagManager } from '@next/third-parties/google'
+import { GoogleAnalyticsTracking } from '@/components/GoogleAnalytics'
 
 const space_grotesk = Space_Grotesk({
   subsets: ['latin'],
@@ -96,9 +97,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 </script>
 
       <body className="bg-white pl-[calc(100vw-100%)] text-black antialiased dark:bg-black dark:text-white">
-      {process.env.NODE_ENV === "production" ? (
+      {/* {process.env.NODE_ENV === "production" ? (
         <GoogleTagManager gtmId="GTM-PRGXHK3D" />
-      ) : null}
+      ) : null} */}
+        <GoogleAnalyticsTracking />
         <ThemeProviders>
           <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
           <SectionContainer>
---
+++ components/GoogleAnalytics.tsx
@@ -0,0 +1,19 @@
+import Script from 'next/script'
+
+export const GoogleAnalyticsTracking = () => {
+  return (
+    <>
+      {/* Global site tag (gtag.js) - Google Analytics */}
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-JNEPSTX4S5" />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-JNEPSTX4S5');
+        `}
+      </Script>
+    </>
+  )
+}
