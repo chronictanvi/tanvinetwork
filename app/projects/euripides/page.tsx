0d40a457124389e7441b8f6a6e148f8808146030
yuchi <chronictanvi@gmail.com>
2024-11-01 13:58:27 -0400

    layout changes

--- app/projects/euripides/page.tsx
+++ app/projects/euripides/page.tsx
@@ -2,7 +2,11 @@
 import Image from 'next/image'
 
 import acaju from './acaju.png'
+import proof from './proof.jpg'
+
 import cherry from './cherry.jpg'
+import herakles from './herakles.png'
+import charactergif from './charactergif.gif'
 
 import CharacterViewer from '@/components/CharacterViewer'
 import FontViewer from '@/components/FontViewer'
@@ -27,93 +31,111 @@ const downloadEuripides = () => {
 export default function Euripides() {
   return (
     <div style={{ fontFamily: 'Euripides' }} className="max-w-8xl mx-auto  dark:bg-black">
-      <div className="mx-auto mt-12 flex max-w-6xl flex-row flex-col gap-16 px-5">
-        <></>
-        <div className="flex basis-2/3 flex-col  pr-16 pt-8 md:space-y-5">
-          <p className="pb-3 text-sm tracking-wider text-zinc-400">OVERVIEW</p>{' '}
-          <h1 className="pb-5 text-3xl font-bold leading-9  text-gray-900 dark:text-gray-100 md:leading-9">
-            Euripides is a reverse stress body typeface designed for Anne Carson's Grief Lessons, a
-            translation of Four Plays by Euripides.
-          </h1>
-          <div className="pb-5">
-            <p className=" pb-2 text-xl font-light">
-              How can the cliche of body text be challenged? What is to translate the form of a
-              glyph — such that the shifts in the weight consider the sentiment of the text? 
-            </p>
-          </div>
-          {/* Download Button */}
-          <div className="flex flex-col gap-4 md:flex-row">
-            <div className="mt-8 ">
-              <a
-                href="/static/fonts/Euripides.woff2" // Link to the font file in the public folder
-                download="Euripides.woff" // Name the file that will be downloaded
-              >
-                <button
-                  className="rounded-lg border-2 border-black	 px-4 py-4 text-black hover:bg-red-400 dark:border-white dark:text-white"
-                  onClick={downloadEuripides}
+      <div className="mx-auto mt-12 flex max-w-6xl flex-col gap-16 px-5">
+        <div className="flex flex-col  gap-10  md:flex-row">
+          <div>
+            <p className="pb-3 text-sm tracking-wider text-zinc-400"> OVERVIEW</p>{' '}
+            <h1 className="pb-5 text-3xl font-bold leading-9  text-gray-900 dark:text-gray-100 md:leading-9">
+              Euripides is a reverse stress body typeface designed for Anne Carson's Grief Lessons,
+              a translation of Four Plays by Euripides.
+            </h1>
+            {/* Download Button */}
+            <div className="flex flex-col gap-4 md:flex-row">
+              <div className="mt-8 ">
+                <a
+                  href="/static/fonts/Euripides.woff2" // Link to the font file in the public folder
+                  download="Euripides.woff" // Name the file that will be downloaded
                 >
-                  Download .woff
-                </button>
-              </a>
+                  <button
+                    className="rounded-lg border-2 border-black	 px-4 py-4 text-black hover:bg-red-400 dark:border-white dark:text-white"
+                    onClick={downloadEuripides}
+                  >
+                    Download .woff
+                  </button>
+                </a>
+              </div>
+
+              <div className="mt-8 ">
+                <a
+                  href="/static/fonts/Euripides.otf" // Link to the font file in the public folder
+                  download="Euripides.otf" // Name the file that will be downloaded
+                >
+                  {' '}
+                  <button
+                    className="rounded-lg border-2 border-black	 px-4 py-4 text-black hover:bg-red-400 dark:border-white dark:text-white"
+                    onClick={downloadEuripides}
+                  >
+                    Download .otf
+                  </button>
+                </a>
+              </div>
             </div>
-            <div className="mt-8 ">
+          </div>
+          <div>
+            <div className=" mb-10 basis-1/3">
+              <p className="pb-3 text-sm tracking-wider text-zinc-400">SCOPE</p>
+              <ul>
+                <li>Type Design</li>
+              </ul>
+            </div>
+            <div className="mb-10 basis-1/3 ">
+              <p className="pb-3 text-sm tracking-wider text-zinc-400">TOOLS</p>
+              <ul>
+                <li>Robofont</li>
+                {/* <li>Proofs made in Adobe InDesign. </li> <li> GitHub for version control. </li> */}
+              </ul>
+            </div>
+            <div className="mb-10 basis-1/3 ">
+              <p className="pb-3 text-sm tracking-wider text-zinc-400">MENTORSHIP</p>
+              <ul>
+                <li>Dai Foldes</li>
+                <li>Victoria Rushton </li>
+              </ul>
+            </div>
+            <div className="mb-10 basis-1/3 ">
               <a
-                href="/static/fonts/Euripides.otf" // Link to the font file in the public folder
-                download="Euripides.otf" // Name the file that will be downloaded
+                className="pb-3 text-sm uppercase tracking-wider text-zinc-400 hover:underline"
+                href="https://fontsinuse.com/search?terms=euripides&page=1"
               >
                 {' '}
-                <button
-                  className="rounded-lg border-2 border-black	 px-4 py-4 text-black hover:bg-red-400 dark:border-white dark:text-white"
-                  onClick={downloadEuripides}
-                >
-                  Download .otf
-                </button>
+                Fonts in Use{' '}
               </a>
+            </div>
+            <div className="mb-10 basis-1/3 ">
+              <p className="pb-3 text-sm tracking-wider text-zinc-400">VERSION / LICENSE </p>
+              <ul>
+                <li>
+                  2.0 — currently under revision! Free for Personal Use. Contact me for Commercial
+                  uses.
+                </li>
+              </ul>
             </div>
           </div>
         </div>
 
-        <div className=" flex flex-col md:flex-row">
-          <div className=" mb-10 basis-1/3">
-            <p className="pb-3 text-sm tracking-wider text-zinc-400">SCOPE</p>
-            <ul>
-              <li>Type Design</li>
-            </ul>
+        <div className="flex flex-col items-end gap-10 md:flex-row">
+          <div className="basis-1/2">
+            {' '}
+            <Image src={herakles} alt="screenshot from anne carson" className="" />
+            {/* <Image src={charactergif} alt="screenshot from anne carson" className="" unoptimized /> */}
           </div>
-          <div className="mb-10 basis-1/3 ">
-            <p className="pb-3 text-sm tracking-wider text-zinc-400">TOOLS</p>
-            <ul>
-              <li>Robofont</li>
-              {/* <li>Proofs made in Adobe InDesign. </li> <li> GitHub for version control. </li> */}
-            </ul>
+          <div className="basis-1/2">
+            <p className="pb-3  text-sm uppercase tracking-wider text-zinc-400"> IDEATION </p>
+            <h1 className="text-2xl"> How can the cliche of body text be challenged? </h1>{' '}
+            <p className=" pt-5 text-lg font-light">
+              {' '}
+              What is to translate the form of a glyph — such that the shifts in the weight consider
+              the sentiment of the text? 
+            </p>
           </div>
-          <div className="mb-10 basis-1/3 ">
-            <p className="pb-3 text-sm tracking-wider text-zinc-400">MENTORSHIP</p>
-            <ul>
-              <li>Dai Foldes</li>
-              <li>Victoria Rushton </li>
-            </ul>
-          </div>
-          <div className="mb-10 basis-1/3 ">
-            <p className="pb-3 text-sm tracking-wider text-zinc-400">VERSION / LICENSE </p>
-            <ul>
-              <li>
-                2.0 — currently under revision! Free for Personal Use. Contact me for Commercial
-                uses.
-              </li>
-            </ul>
-          </div>
-          {/* <div className="basis-1/2">
-            <p className="pb-3 text-sm tracking-wider text-zinc-400">LICENSE</p>
-            <p> </p>
-          </div> */}
         </div>
+
         <FontViewer />
+        <CharacterViewer />
       </div>
-      <CharacterViewer />
       <div className="mx-auto mt-12 flex max-w-6xl  flex-col gap-16 px-5">
-        <p className="pb-3 text-center text-sm tracking-wider text-zinc-400"> IN USE </p>
-        <div className="mx-auto flex max-w-4xl flex-col gap-10  md:flex-row">
+        <p className="pb-3 text-center text-sm uppercase tracking-wider text-zinc-400"> IN USE </p>
+        <div className="mx-auto flex max-w-6xl flex-col gap-10  md:flex-row">
           <div className="basis-1/2">
             <Image src={acaju} alt="Stamp" className="" unoptimized />
           </div>
@@ -122,6 +144,7 @@ export default function Euripides() {
             <Image src={cherry} alt="Poster" className="" unoptimized />
           </div>
         </div>
+        <Image src={proof} alt="Poster" className="" unoptimized />
       </div>
     </div>
   )
--- components/FontViewer.tsx
+++ components/FontViewer.tsx
@@ -3,7 +3,7 @@
 import { useState } from 'react'
 
 const FontViewer = () => {
-  const [previewText, setPreviewText] = useState('ABCXYZ')
+  const [previewText, setPreviewText] = useState('HERAKLES')
   const [fontSize, setFontSize] = useState(40)
   const [trackSize, setTrackSize] = useState(0)
 
@@ -11,36 +11,37 @@ const FontViewer = () => {
   const handleSizeChange = (e) => setFontSize(e.target.value)
 
   return (
-    <div style={{ fontFamily: 'Euripides' }} className=" py-16 ">
+    <div style={{ fontFamily: 'Euripides' }} className=" pt-16 ">
       {/* <p className=" pb-3 text-center text-sm uppercase tracking-wider text-zinc-400">
         Font Preview
       </p> */}
-      <div className="flex flex-col md:flex-row">
-        <div>
-          <input
-            type="text"
-            value={previewText}
-            onChange={handleTextChange}
-            placeholder="Type to preview..."
-            style={{ padding: '10px', fontSize: '20px', background: 'none' }}
-            className="mr-12 "
-          />
-        </div>
-        <div className="my-5 md:my-0">
-          <label className="">
-            Font Size:
+      <div>
+        <div className="flex flex-col justify-center md:flex-row">
+          <div>
             <input
-              type="range"
-              min="10"
-              max="150"
-              value={fontSize}
-              onChange={handleSizeChange}
-              className="mx-4"
+              type="text"
+              value={previewText}
+              onChange={handleTextChange}
+              placeholder="Type to preview..."
+              style={{ padding: '10px', fontSize: '20px', background: 'none' }}
+              className="mr-12 "
             />
-            {fontSize}px
-          </label>
-        </div>
-        {/* <label className="mr-5 px-5">
+          </div>
+          <div className="my-5 md:my-0">
+            <label className="">
+              Font Size:
+              <input
+                type="range"
+                min="10"
+                max="200"
+                value={fontSize}
+                onChange={handleSizeChange}
+                className="mx-4"
+              />
+              {fontSize}px
+            </label>
+          </div>
+          {/* <label className="mr-5 px-5">
           Tracking:
           <input
             type="range"
@@ -52,13 +53,19 @@ const FontViewer = () => {
           />
           {trackSize}
         </label> */}
-      </div>
-      <div className="character-preview" style={{ fontSize: `${fontSize}px`, marginTop: '20px' }}>
-        {previewText.split('').map((char, index) => (
-          <span key={index} style={{}}>
-            {char}
-          </span>
-        ))}
+        </div>
+        <div className="flex justify-center">
+          <div
+            className="character-preview "
+            style={{ fontSize: `${fontSize}px`, marginTop: '20px' }}
+          >
+            {previewText.split('').map((char, index) => (
+              <span key={index} style={{}}>
+                {char}
+              </span>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   )
