f823cb137a40e3c274bf3fb6622647502d529f9a
yuchi <chronictanvi@gmail.com>
2025-03-01 18:02:22 -0500

    fixing responsiveness

--- layouts/Posters.tsx
+++ layouts/Posters.tsx
@@ -25,7 +25,7 @@ export default function PostLayout({ content, next, prev, children }: LayoutProp
       <article className="px-10 md:px-20">
         <div>
           <header>
-            <div className="space-y-1 border-b border-gray-200 pb-10 text-center dark:border-gray-700">
+            <div className="space-y-1 ">
               {/* <dl>
                 <div>
                   <dt className="sr-only">Published on</dt>
@@ -37,8 +37,8 @@ export default function PostLayout({ content, next, prev, children }: LayoutProp
               <div>{/* <PageTitle>{title}</PageTitle> */}</div>
             </div>
           </header>
-          <div className="grid-rows-[auto_1fr] divide-y divide-gray-200 pb-8 dark:divide-gray-700 xl:divide-y-0">
-            <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
+          <div className="grid-rows-[auto_1fr] ">
+            <div className="">
               <div className="prose max-w-none pb-8 pt-10 dark:prose-invert">{children}</div>
             </div>
             {/* {siteMetadata.comments && (
