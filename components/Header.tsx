424ef4fca53e011dced3df803d4bf19f9169f483
yuchi <chronictanvi@gmail.com>
2025-08-19 22:29:28 -0400

    small tweaks

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,6 +4,8 @@ import Link from './Link'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
 import SearchButton from './SearchButton'
+import headernav from '../public/static/favicons/android-chrome-512x512.png'
+import Image from 'next/image'
 
 const Header = () => {
   return (
@@ -11,10 +13,13 @@ const Header = () => {
       <div>
         <Link href="/" aria-label={siteMetadata.headerTitle}>
           <div className="flex items-center justify-between">
-            <div className="mr-3">{/* <Logo /> */}</div>
+            <div className="hidden h-8 w-8 sm:block">
+              {' '}
+              <Image src={headernav} alt="Icon for Homepage" />{' '}
+            </div>
             {typeof siteMetadata.headerTitle === 'string' ? (
-              <div className="hidden h-6 text-2xl font-semibold sm:block">
-                {siteMetadata.headerTitle}
+              <div className="hidden h-8 w-8 text-2xl font-semibold sm:block">
+                {/* {siteMetadata.headerTitle} */}
               </div>
             ) : (
               siteMetadata.headerTitle
