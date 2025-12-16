8e5e7ac40e6e5f432ccb96d72f972dd732ff054f
chronictanvi <42870186+chronictanvi@users.noreply.github.com>
2025-12-16 18:55:14 -0500

    restoring author layour and adding new arrow to details

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -19,28 +19,30 @@ export default function AuthorLayout({ children, content }: Props) {
             About
           </h1> */}
         </div>
-        <div id="badge" className="flex flex-col items-center space-x-2 pt-8">
-          {avatar && (
-            <Image
-              src={avatar}
-              alt="avatar"
-              width={192}
-              height={192}
-              className="h-48 w-48 rounded-full"
-            />
-          )}
-          <h3 className="pb-2 pt-4 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
-          <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
-          <div className="text-gray-500 dark:text-gray-400">{company}</div>
-          <div className="flex space-x-3 pt-6">
-            <SocialIcon kind="mail" href={`mailto:${email}`} />
-            <SocialIcon kind="github" href={github} />
-            <SocialIcon kind="linkedin" href={linkedin} />
-            <SocialIcon kind="x" href={twitter} />
+        <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
+          <div id="badge" className="flex flex-col items-center space-x-2 pt-8">
+            {avatar && (
+              <Image
+                src={avatar}
+                alt="avatar"
+                width={192}
+                height={192}
+                className="h-48 w-48 rounded-full"
+              />
+            )}
+            <h3 className="pb-2 pt-4 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
+            <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
+            <div className="text-gray-500 dark:text-gray-400">{company}</div>
+            <div className="flex space-x-3 pt-6">
+              <SocialIcon kind="mail" href={`mailto:${email}`} />
+              <SocialIcon kind="github" href={github} />
+              <SocialIcon kind="linkedin" href={linkedin} />
+              <SocialIcon kind="x" href={twitter} />
+            </div>
           </div>
-        </div>
-        <div className="prose max-w-none pb-8 pt-8 dark:prose-invert w-full">
-          {children}
+          <div className="prose max-w-none pb-8 pt-8 dark:prose-invert xl:col-span-2">
+            {children}
+          </div>
         </div>
       </div>
     </>
